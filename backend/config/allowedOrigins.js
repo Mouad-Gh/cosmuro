5cf20608c0f75e07640325b9eba4427c725190dc
Mouad-Gh <Mouad.ghouichat@gmail.com>
2022-12-30 19:17:58 +0100

    adding sales post request

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,6 +1,9 @@
 
 //the origins that we want to access our api
 const allowedOrigins = [
+    'https://cosmuro-ppeh.vercel.app',
+    'https://localhost:4000',
+    'http://localhost:4000',
     'http://127.0.0.1:3000',
     'http://localhost:3000',
     'https://cosmuro-ppeh.vercel.app',
