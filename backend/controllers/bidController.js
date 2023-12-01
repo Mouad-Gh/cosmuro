84733eba1b8a88babc7de93add3409e09dc32bd5
Mouad-Gh <72326045+Mouad-Gh@users.noreply.github.com>
2023-12-01 23:36:40 +0100

    Update bidController.js

--- backend/controllers/bidController.js
+++ backend/controllers/bidController.js
@@ -6,8 +6,8 @@ const addBid = async (req, res)=>{
     const { tx, blockNo, contract, token, bidder, bidPrice, bidTime } = req.body;
     //console.log(contract)
     try {
-        const Bid = await Bid.create({ tx, blockNo, contract, token, bidder, bidPrice, bidTime });
-        res.status(200).json({ Bid });
+        const bid = await Bid.create({ tx, blockNo, contract, token, bidder, bidPrice, bidTime });
+        res.status(200).json({ bid });
     } catch (error) {
         res.status(400).json({err : error.message});
     }
@@ -16,8 +16,8 @@ const addBid = async (req, res)=>{
 const getBids = async (req, res) => {
     const search = req.query.name || "";
     try {
-        const Bids = await Bid.find({tx: {$regex:search, $options: 'i'}}).sort({createdAt: -1}).limit(100);
-        res.status(200).json({count:Bids.length, Bids});
+        const bids = await Bid.find({tx: {$regex:search, $options: 'i'}}).sort({createdAt: -1}).limit(100);
+        res.status(200).json({count:bids.length, bids});
     } catch (error) {
         res.status(400).json({err : error.message});
     }
