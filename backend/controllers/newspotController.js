f03d011414ca527cbee03ca0e0da5aafd02bd663
Mouad-Gh <72326045+Mouad-Gh@users.noreply.github.com>
2023-12-01 23:30:34 +0100

    Update newspotController.js

--- backend/controllers/newspotController.js
+++ backend/controllers/newspotController.js
@@ -6,8 +6,8 @@ const addNewSpot = async (req, res)=>{
     const { tx, blockNo, contract, token, creator, startPrice, duration, prize } = req.body;
     //console.log(contract)
     try {
-        const NewSpot = await NewSpot.create({ tx, blockNo, contract, token, creator, startPrice, duration, prize });
-        res.status(200).json({ NewSpot });
+        const newSpot = await NewSpot.create({ tx, blockNo, contract, token, creator, startPrice, duration, prize });
+        res.status(200).json({ newSpot });
     } catch (error) {
         res.status(400).json({err : error.message});
     }
@@ -16,8 +16,8 @@ const addNewSpot = async (req, res)=>{
 const getNewSpots = async (req, res) => {
     const search = req.query.name || "";
     try {
-        const NewSpots = await NewSpot.find({tx: {$regex:search, $options: 'i'}}).sort({createdAt: -1}).limit(100);
-        res.status(200).json({count:NewSpots.length, NewSpots});
+        const newSpots = await NewSpot.find({tx: {$regex:search, $options: 'i'}}).sort({createdAt: -1}).limit(100);
+        res.status(200).json({count:newSpots.length, newSpots});
     } catch (error) {
         res.status(400).json({err : error.message});
     }
